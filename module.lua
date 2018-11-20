dbbc347d8aa99fb173c2ee943ce8ed5cad704ff0
Max Pasichnyk <maximmkaaa@gmail.com>
2018-11-20 18:24:01 +0200

    fix import context

--- module.lua
+++ module.lua
@@ -2,14 +2,26 @@ _G.modules = {}
 _G.module = function()
     local file = debug.getinfo(2,'S').short_src
     assert(_G.modules[file] == nil, 'module \''..file..'\' is already exist')
-    local module = setmetatable({}, { __index = _G })
+    local module = {}
+    local L = {}
+
+    setmetatable(module, { 
+        __index = function(self, k)
+            if k == '_L' then
+                return L
+            end
+
+            return L[k] or _G[k]
+        end
+    })
     setfenv(2, module)
+    
     _G.modules[file] = module
 end
 
-_G.import = function(name)
+_G.import = function(name, env)
     local module = _G.modules[name..'.lua']
-
+    
     if not module then
         local status, result = pcall(loadfile, name..'.lua')
 
@@ -19,8 +31,10 @@ _G.import = function(name)
         assert(module, 'module \''..name..'\' not found')
     end
 
-    local env = getfenv(2)
+    env = env or getfenv(2)._L
     for k, v in pairs(module) do
         env[k] = v
     end
+
+    return env
 end
