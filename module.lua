4e6dce785653bfaba3288715154c59146b756663
Max Pasichnyk <maximmkaaa@gmail.com>
2018-12-28 12:03:07 +0200

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,40 +1,44 @@
-_G.modules = {}
-_G.module = function()
-    local file = debug.getinfo(2,'S').short_src
-    assert(_G.modules[file] == nil, 'module \''..file..'\' is already exist')
+local module = {}
+function module.new()
+    local _L = {}
 
-    local L = {}
-
-    local module = setmetatable({}, { 
-        __index = function(self, k)
-            if k == '_L' then
-                return L
-            end
-
-            return L[k] or _G[k]
+    local function get(self, key)
+        if key == '_L' then
+            return _L
         end
-    })
-    setfenv(2, module)
-    
-    _G.modules[file] = module
-end
-
-_G.import = function(name, packed)
-    local module = _G.modules[name..'.lua']
-    
-    if not module then
-        local status, result = pcall(loadfile, name..'.lua')
-
-        assert(status, result)
-        assert(pcall(setfenv(result, setmetatable({}, { __index = _G }))))
-        module = _G.modules[name..'.lua']
-        assert(module, 'module \''..name..'\' not found')
+        return _L[key] or _G[key]
     end
 
-    local env = packed and {} or getfenv(2)._L
-    for k, v in pairs(module) do
-        env[k] = v
+    local function set(self, key, value)
+        _L[key] = value
     end
 
-    return env
+    return setmetatable({}, { __index = get, __newindex = set })
 end
+
+local modules = {}
+function module.load(path, L)
+    local env = modules[path]
+
+    if not env then
+        env = module.new()
+        assert(pcall(setfenv(loadfile(path..'.lua'), env)))
+        modules[path] = env
+    end
+
+    L = L or {}
+    for k, v in pairs(env._L) do
+        L[k] = v
+    end
+    return L
+end
+
+_G.module = module
+_G.import = function(path)
+    module.load(path, getfenv(2)._L)
+end
+_G.export = function(name, value)
+    _G[name] = value or getfenv(2)[name]
+end
+
+setfenv(3, module.new())
